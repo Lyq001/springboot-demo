f442c61f9a76a453b5bf8516cf55d4aef66b1787
Lyq001 <13761753949@163.com>
2019-06-21 18:15:16 +0800

    在User实体类利用Jackson演绎注解对字段进行处理

--- Springboot-demo/src/main/java/com/lyq/entity/User.java
+++ Springboot-demo/src/main/java/com/lyq/entity/User.java
@@ -2,11 +2,22 @@ package com.lyq.entity;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 public class User {
 	private String name;
+	@JsonIgnore 
+	//该注释是忽略不显示
 	private String password;
+	@JsonFormat(pattern = "yyyy-MM-dd",locale = "zh",timezone = "GMT+8")
+	//该注释是格式化日期类型，2010-01-01  中国  东八区
 	private Date birthday;
 	private Integer age;
+	@JsonInclude(Include.NON_NULL)
+	//该字段是如果值为null，不反回给前台。
 	private String desc;
 	public String getName() {
 		return name;
