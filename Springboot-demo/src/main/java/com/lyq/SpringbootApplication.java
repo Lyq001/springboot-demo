8a7239e582a943f7f7445ec6b495b5ddd553adb1
Lyq001 <13761753949@163.com>
2019-06-24 14:16:09 +0800

    init

--- Springboot-demo/src/main/java/com/lyq/SpringbootApplication.java
+++ Springboot-demo/src/main/java/com/lyq/SpringbootApplication.java
@@ -2,9 +2,11 @@ package com.lyq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
